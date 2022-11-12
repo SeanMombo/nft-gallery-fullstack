b8d0e73620838f3f46ee3a238e70d5c1176410a0
sean mombo <sean@reflektor.digital>
2022-11-12 14:10:31 -0500

    added title header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import SearchBar from '../components/SearchBar/Searchbar'
 import NftGallery from '../components/NftGallery/NftGallery'
@@ -8,6 +7,9 @@ import ErrorMessage from '../components/ErrorMessage/ErrorMessage'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Nft Gallery</title>
+      </Head>
       <h1>NFT Gallery</h1>
       <h4>By: Sean Mombo</h4>
       <SearchBar/>
