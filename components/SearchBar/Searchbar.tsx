c8071d38bdea246462a874be910ca62dd2918a44
sean mombo <sean@reflektor.digital>
2022-11-12 00:44:28 -0500

    updated searchbar styling and shortened api url

--- components/SearchBar/Searchbar.tsx
+++ components/SearchBar/Searchbar.tsx
@@ -1,12 +1,12 @@
 //create a react search bar component with button
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import useNftStore from '../../store/nfts';
 import styles from '../../styles/SearchBar.module.css';
 
 const SearchBar = () => {
   // get store state and actions
-  const {setNfts, setError} = useNftStore((state) => ({ setNfts: state.setNfts, setError: state.setError }));
+  const { setNfts, setError } = useNftStore((state) => ({ setNfts: state.setNfts, setError: state.setError }));
 
   // create state for search bar
   const [address, setAddress] = useState('');
@@ -14,9 +14,8 @@ const SearchBar = () => {
   //create a function to handle the search button click
   const onSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    axios.get(`http://localhost:3001/api/nft/${address}`)
+    axios.get(`/api/nft/${address}`)
     .then((res) => {
-      console.info(res.data)
       setNfts(res.data);
     })
     .catch((err) => {
@@ -24,7 +23,6 @@ const SearchBar = () => {
       setNfts([]);
     })
   };
-
   //create a function to handle the search bar input and store the value in state
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const input: HTMLInputElement = event.currentTarget;
